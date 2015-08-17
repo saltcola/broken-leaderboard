5bed8f7aa7bf86fcb24278a9d83ce21a5657c6bd
Krishna R. Sampath <Krishna.R.Sampath@gmail.com>
2015-08-17 18:06:42 -0400

    Initial commit

---
+++ leaderboard.js
@@ -0,0 +1,66 @@
+// Set up a collection to contain player information. On the server,
+// it is backed by a MongoDB collection named "players".
+
+Players = new Mongo.Collection("players");
+
+if (Meteor.isClient) {
+  Template.leaderboard.helpers({
+    players: function () {
+      return Players.find({}, { sort: { score: -1, name: 1 } });
+    },
+    selectedName: function () {
+      var player = Players.findOne(Session.get("selectedPlayer"));
+      return player && player.name;
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .inc': function () {
+      Players.update(Session.get("selectedPlayer"), {$inc: {score: 5}});
+    },
+    'click .dec': function () {
+      Players.update(Session.get("selectedPlayer"), {$inc: {score: -5}});
+    },
+    'click .delete': function () {
+      Players.remove(Session.get("selectedPlayer"));
+    },
+    'click #addPlayer': function(event, template) {
+      var input = template.find("#inputPlayerName").value;
+      Players.insert({
+          name: input,
+          score: 0
+        });
+
+      template.find("#inputPlayerName").value="";
+    }
+  });
+
+  Template.player.helpers({
+    selected: function () {
+      return Session.equals("selectedPlayer", this._id) ? "selected" : '';
+    }
+  });
+
+  Template.player.events({
+    'click': function () {
+      Session.set("selectedPlayer", this._id);
+      console.log("Player's name is " + Session.get("selectedPlayer"));
+    }
+  });
+}
+
+// On server startup, create some players if the database is empty.
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Players.find().count() === 0) {
+      var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
+                   "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
+      _.each(names, function (name) {
+        Players.insert({
+          name: name,
+          score: Math.floor(Random.fraction() * 10) * 5
+        });
+      });
+    }
+  });
+}
